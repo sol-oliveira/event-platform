63aeff350db790c5c890848c832bfb9c1facc600
Sol <sol.oliveira@elevenfinancial.com>
2022-06-25 20:49:54 -0300

    aula-4: add variáveis de ambiente

--- event-platform/src/lib/apollo.ts
+++ event-platform/src/lib/apollo.ts
@@ -1,7 +1,10 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4q0znyd2hcr01xxe9hr67i7/master',
+  uri: import.meta.env.VITE_API_URL_GRAPHCMS,
+  headers: {
+    'Authorization': `Bearer ${import.meta.env.VITE_ACCESS_TOKEN_GRAPHCMS}`
+  },
   cache: new InMemoryCache(),
   
  })
